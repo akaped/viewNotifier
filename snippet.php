0e152a8a80d37c6eb3782719c2ed11d309204d2f
Pietro Boccaletto <pboccaletto@genesilico.pl>
2019-04-26 19:48:33 +0200

    v1

---
+++ snippet.php
@@ -0,0 +1,37 @@
+<?php
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+
+function sendMessage($chatID, $messaggio, $token) {
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+$data = "\n".getUserIpAddr()." ".date("m/d/y G.i:s", time());
+$token = "YOU NEED TO INSERT HERE YOUR API TOKEN FOR YOUR BOT";
+$chatid = "YOU NEED TO INSERT HERE YOUR CHAT ID";
+sendMessage($chatid, "New Visitor for WEBSITE.com - ".$data, $token);
+
+?>
